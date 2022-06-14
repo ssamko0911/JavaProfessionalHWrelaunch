c0bd8eb5c61da5b1abd3c4da9037f0503ff91e49
samko.s <ssamko091184@gmail.com>
2022-06-15 00:41:33 +0300

    task 002 done.

---
+++ src/main/java/hw001/task002/Main.java
@@ -0,0 +1,26 @@
+package hw001.task002;
+/*
+Задание 2
+Вывод на экран элементов List: Создать список, заполнить
+его на 10 элементов и вывести на экран содержимое используя iterator.
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static final int LENGTH_OF_LIST = 10;
+
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 0; i < LENGTH_OF_LIST; i++) {
+            integerList.add((int) ((Math.random() * 89) + 10));
+        }
+        Iterator<Integer> integerIterator = integerList.iterator();
+        while (integerIterator.hasNext()) {
+            Integer element = integerIterator.next();
+            System.out.print(element + " | ");
+        }
+    }
+}
